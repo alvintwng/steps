f2bb0a0d784ec3fcc1ee5dd6117a3ea7994b0138
AlvinTWNg <alvintwng@gmail.com>
2021-05-21 15:45:24 +0800

    data.sql

---
+++ AuthEmp/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+Insert into ROLE (NAME) values ('ADMIN');
+Insert into ROLE (NAME) values ('MANAGER');
+Insert into ROLE (NAME) values ('USER');
+Insert into EMPLOYEE (EMPNAME,PASSWORD) values ('admin','$2a$10$u97XKnz.e2i.C9ADAgkMYuZvtpnKgVA6R8a9BJglIBLaghkXGCDQq');
+Insert into EMP_ROLES (EMPID,ROLEID) values (1,1);
+Insert into EMPLOYEE (EMPNAME,PASSWORD) values ('ntuc','$2a$10$60iSTtJQWjz08JJElVOjM.vhbBMHPoozv1MfroZxkBHjDAjnRG8Tm');
+Insert into EMP_ROLES (EMPID,ROLEID) values (2,1);
+Insert into EMP_ROLES (EMPID,ROLEID) values (2,2);
+Insert into EMP_ROLES (EMPID,ROLEID) values (2,3);
