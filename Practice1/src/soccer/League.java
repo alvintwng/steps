21d30a1930601f7fed0636feea1c5379daaaaf8c
AlvinTWNg <alvintwng@gmail.com>
2021-06-14 14:39:45 +0800

    pr103 - added comments

--- Practice1/src/soccer/League.java
+++ Practice1/src/soccer/League.java
@@ -1,3 +1,4 @@
+//Practice 6-2: Creating a Soccer Game
 package soccer;
 
 public class League {
@@ -28,20 +29,31 @@ public class League {
         team2.playerArray[2] = new Player();
         team2.playerArray[2].playerName = "Rafael Sabatini";
 
-        
+        //create a Game and populate the homeTeam and awayTeam attributes. 
         Game currGame = new Game();
         currGame.homeTeam = team1;
         currGame.awayTeam = team2;
+        
+        //Create a Goal object to give the home team a 1–0 lead. 
         Goal goal1 = new Goal();
         goal1.thePlayer = currGame.homeTeam.playerArray[2];
         goal1.theTeam = currGame.homeTeam;
         goal1.theTime = 55;
+        
+        //Put this Goal object in a Goal array, and then assign this Goal array 
+        //to the goals attribute of the Game object.
         Goal[] theGoals = {goal1};
         currGame.goals = theGoals;
         
+        //Print out the score of the game
         System.out.println("Goal scored after " + 
                 currGame.goals[0].theTime + " mins by " +
                 currGame.goals[0].thePlayer.playerName + " of " +
                 currGame.goals[0].theTeam.teamName);
     }   
 }
+
+/* console
+run:
+Goal scored after 55.0 mins by Geoffrey Chaucer of The Greens
+*/
