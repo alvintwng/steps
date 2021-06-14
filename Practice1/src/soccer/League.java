1f26b476594094a189aeb239f5d23f2baf1a5fea
AlvinTWNg <alvintwng@gmail.com>
2021-06-14 15:22:16 +0800

    pr104 - StringBuilder, matches, split, append

--- Practice1/src/soccer/League.java
+++ Practice1/src/soccer/League.java
@@ -3,7 +3,8 @@ package soccer;
 public class League {
 
     public static void main(String[] args) {
-  
+        
+        // Start of code that builds teams
         // Create team1      
         Player player1 = new Player();
         player1.playerName = "George Eliot";
@@ -27,8 +28,10 @@ public class League {
         team2.playerArray[1].playerName = "Robbie Burns";
         team2.playerArray[2] = new Player();
         team2.playerArray[2].playerName = "Rafael Sabatini";
-
         
+        // End of code that builds teams
+        
+    
         Game currGame = new Game();
         currGame.homeTeam = team1;
         currGame.awayTeam = team2;
@@ -43,5 +46,26 @@ public class League {
                 currGame.goals[0].theTime + " mins by " +
                 currGame.goals[0].thePlayer.playerName + " of " +
                 currGame.goals[0].theTeam.teamName);
+        
+        
+
+        for (Player thePlayer: team2.playerArray) {
+            //System.out.println(thePlayer.playerName);
+            if (thePlayer.playerName.matches(".*Sab.*")) {
+                System.out.println("Found " + thePlayer.playerName);
+                System.out.println("Last name is " + thePlayer.playerName.split(" ")[1]);
+            }
+        }
+        
+        StringBuilder familyNameFirst = new StringBuilder();
+        for (Player thePlayer: team1.playerArray) {
+            String[] name = thePlayer.playerName.split(" ");
+            familyNameFirst.append(name[1]);
+            familyNameFirst.append(", ");
+            familyNameFirst.append(name[0]);
+            System.out.println(familyNameFirst);
+            familyNameFirst.delete(0,familyNameFirst.length());
+            
+        } 
     }   
 }
