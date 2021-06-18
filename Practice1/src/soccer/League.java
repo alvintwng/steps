4cfcddb557985584b52b7db8f5b18ba929348463
AlvinTWNg <alvintwng@gmail.com>
2021-06-18 22:05:26 +0800

    pr112 - add comments

--- Practice1/src/soccer/League.java
+++ Practice1/src/soccer/League.java
@@ -1,3 +1,4 @@
+// Practice 11-1: Iterating Through Data
 package soccer;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ public class League {
         
         League theLeague = new League();
 
+        //6 Modified the call to getTeams to pass in team names and team size.
         Team[] theTeams = theLeague.createTeams("The Robins,The Crows,The Swallows", 5);
         Game[] theGames = theLeague.createGames(theTeams);
 
@@ -22,10 +24,13 @@ public class League {
 
     }
 
+    // 5. more general-purpose method by passing in team names and team sizes.
     public Team[] createTeams(String teamNames, int teamSize) {
-
+        
+        // 4b. Instantiate a new PlayerDatabase object
         PlayerDatabase playerDB = new PlayerDatabase();
-
+        
+        // 5b iterate through however many teams have been specified.
         StringTokenizer teamNameTokens = new StringTokenizer(teamNames, ",");
         Team[] theTeams = new Team[teamNameTokens.countTokens()];
         for (int i = 0; i < theTeams.length; i++) {
@@ -33,12 +38,29 @@ public class League {
                 playerDB.getTeam(teamSize));
         }
 
+        /* Practice 11-1. Remove the code below that creates The Greens and The Reds */
+
+        /* Practice 11-1. Remove the above code that creates The Greens and The Reds */
+        
+        // 4c. instantiate team1 and team2 to use playerDB for the players. 
+//        Team team1 = new Team("The Greens", playerDB.getTeam(3));
+//        Team team2 = new Team("The Reds", playerDB.getTeam(3));
+//        Team[] theTeams = {team1, team2};
 
         return theTeams;
     }
 
+    // 7. Generate All-Play-All Set of Games
     public Game[] createGames(Team[] theTeams) {
-
+        /*
+        Game theGame = new Game(theTeams[0], theTeams[1]);
+        Game theGame2 = new Game(theTeams[1], theTeams[0]);
+        Game theGame3 = new Game(theTeams[0], theTeams[1]);
+        Game theGame4 = new Game(theTeams[1], theTeams[0]);
+        Game[] theGames = {theGame, theGame2, theGame3, theGame4};
+        */
+        
+        // 7b.Instantiate an ArrayList to hold the games that created
         ArrayList<Game> theGames = new ArrayList();
         for (Team homeTeam: theTeams) {
             for (Team awayTeam: theTeams) {
@@ -48,6 +70,7 @@ public class League {
             } 
         }
 
+        //7f. return an array, not an ArrayList.
         return (Game[]) theGames.toArray(new Game[1]);
     }
     
@@ -73,3 +96,41 @@ public class League {
     }
 
 }
+/* console
+run:
+The Robins vs. The Crows
+Goal scored after 3.0 mins by Liam O'Flaherty of The Robins
+The Robins win (1 - 0) 
+
+The Robins vs. The Swallows
+It's a draw! (0 - 0) 
+
+The Crows vs. The Robins
+Goal scored after 1.0 mins by O. Henry of The Crows
+Goal scored after 2.0 mins by Brian Moore of The Robins
+Goal scored after 7.0 mins by Brian Moore of The Robins
+Goal scored after 65.0 mins by Frank O'Connor of The Robins
+Goal scored after 69.0 mins by Agatha Christie of The Robins
+Goal scored after 79.0 mins by Wilkie Collins of The Crows
+The Robins win (2 - 4) 
+
+The Crows vs. The Swallows
+Goal scored after 46.0 mins by Leo Tolstoy of The Swallows
+Goal scored after 70.0 mins by James Joyce of The Crows
+It's a draw! (1 - 1) 
+
+The Swallows vs. The Robins
+Goal scored after 5.0 mins by Arthur Conan Doyle of The Robins
+Goal scored after 88.0 mins by Frank O'Connor of The Robins
+The Robins win (0 - 2) 
+
+The Swallows vs. The Crows
+It's a draw! (0 - 0) 
+
+
+Team Points
+The Robins : 4 : 7
+The Crows : 2 : 3
+The Swallows : 3 : 1
+Winner of the League is The Robins
+*/
--- Practice1/src/utility/PlayerDatabase.java
+++ Practice1/src/utility/PlayerDatabase.java
@@ -1,3 +1,5 @@
+// Practice 11-1: Iterating Through Data
+
 package utility;
 
 import java.util.ArrayList;
@@ -5,22 +7,27 @@ import java.util.StringTokenizer;
 import soccer.Player;
 
 public class PlayerDatabase {
-
+    
+    /* Practice 11-1. 2a.Declare an ArrayList here */
     private ArrayList <Player> players;
-
+    
+    /* Practice 11-1. 2e.Add Constructor here */
     public PlayerDatabase() {
+        //2d. to iterate through the names and add each to the ArrayList. 
         StringTokenizer authorTokens = new StringTokenizer(authorList, ",");
         players = new ArrayList ();
         while (authorTokens.hasMoreTokens()) {
             players.add(new Player(authorTokens.nextToken()));
         }
     }
-
+    
+    // 3. method to return an arbitrarily sized team
     public Player[] getTeam(int numberOfPlayers) {
         Player[] teamPlayers = new Player[numberOfPlayers];
         for (int i = 0; i < numberOfPlayers; i++) {
             int playerIndex = (int) (Math.random() * players.size());
             teamPlayers[i] = players.get(playerIndex);
+            // 3e. Remove the player just selected from the players ArrayList
             players.remove(playerIndex);
         }
         return teamPlayers;
