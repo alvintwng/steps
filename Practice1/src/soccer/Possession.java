5b97a7cf52f1e697014965ee1d187839733c2882
AlvinTWNg <alvintwng@gmail.com>
2021-06-24 23:00:50 +0800

    pr116: added comments

--- Practice1/src/soccer/Possession.java
+++ Practice1/src/soccer/Possession.java
@@ -2,6 +2,8 @@ package soccer;
 
 public class Possession extends GameEvent {
 
+    /* Practice 13-1. Add a toString() method */
+    //2b. polymorphically 
     public String toString() {
         return "Possession";
     }
