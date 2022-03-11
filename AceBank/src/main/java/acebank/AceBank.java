e18fecdbd710ac28c578174aa9fc780130fb3fbe
AlvinTWNg <alvintwng@gmail.com>
2022-03-11 15:14:07 +0800

    MySql connection

---
+++ AceBank/src/main/java/acebank/AceBank.java
@@ -0,0 +1,18 @@
+/* 220311 1212
+ * CoreBanking
+ */
+package acebank;
+
+/**
+ *
+ * @author antw
+ */
+public class AceBank {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testing mySql: ");
+        SqlConnect.preTest();;
+    }
+    
+}
---
+++ AceBank/src/main/java/acebank/SqlConnect.java
@@ -0,0 +1,55 @@
+/* 220311 1440
+ * mySQL connection default
+ * 
+ * Dependencies added: mysql-connector-java > version 8.0.22
+ * Right click at Dependencies, Add Dependency, Query: mysql
+ */
+package acebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ *
+ * @author antw
+ */
+public class SqlConnect {
+    
+    public static Statement init() throws Exception {
+        Connection conn = initConn();
+        return conn.createStatement();
+    }
+    
+    public static Connection initConn() throws Exception {
+        Connection conn = null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager
+            .getConnection("jdbc:mysql://localhost:3306/aceBank?"
+                    + "useTimezone=true&serverTimezone=UTC&"
+                    + "user=root&password=mysql_80");
+        return conn;
+    }
+    
+    public static void preTest() {
+        try {
+            SqlConnect.init();
+            System.out.println("mySql connected!");
+        } catch (Exception e) {
+            System.out.println(" Exception here ....  " + e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+        preTest();
+    }
+}
+
+/*
+mySQL pre-installed and up.
+
+sql query:
+CREATE DATABASE  IF NOT EXISTS `aceBank`;
+
+do a refresh by click home and back. aceBank database will be shown.
+*/
